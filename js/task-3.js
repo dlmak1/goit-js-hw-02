2fd2d56e1197f6f29f5c67360593f5cf23154606
Dmytro <dlmakseverin@gmail.com>
2025-04-09 14:13:00 +0300

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,12 @@
 console.log('\nTask-3');
+
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+}
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SPAM detection")); // true
